cebb093b4b19076cc589a673e864cdef0cdf62aa
Jose Antonio <jciccio@gmail.com>
2025-09-04 18:10:48 -0600

    Ejercicios ciclos

--- Bisiesto.java
+++ Bisiesto.java
@@ -5,7 +5,6 @@ public class Bisiesto{
 		boolean bisiesto = false;
 		//Un año es bisiesto si es divisible entre 4 
 		//y no es divisible entre 100, o si es divisible entre 400.
-
 		if(anyo >= 0){
 			if((anyo % 4 == 0 && anyo % 100 != 0) || anyo % 400 ==0){
 				bisiesto = true;
